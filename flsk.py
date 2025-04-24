c469084c7ee5e15deb97f730f1acf6901ff59378
ghlzxcvert <wildrage2015@gmail.com>
2025-04-24 10:15:55 +0300

    second

--- flsk.py
+++ flsk.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return '<h1>ZALUPA</h1>'
 
+@app.route('info')
+def info():
+    return '<h1>info</h1>'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
